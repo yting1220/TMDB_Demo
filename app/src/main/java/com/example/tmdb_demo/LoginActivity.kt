430beef5d4801efd854b941f248f4483edb3aee2
yongting <japan851220@gmail.com>
2021-08-18 17:27:40 +0800

    VC 1 登入畫面
    基本元件

--- app/src/main/java/com/example/tmdb_demo/LoginActivity.kt
+++ app/src/main/java/com/example/tmdb_demo/LoginActivity.kt
@@ -3,9 +3,14 @@ package com.example.tmdb_demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "Login"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
