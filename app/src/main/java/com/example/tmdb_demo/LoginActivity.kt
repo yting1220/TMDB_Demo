00aff682b4910bc5bd9df23bed08c61107aff2fa
Cong Hua <a4704704@gmail.com>
2021-08-18 23:41:25 +0800

    VC 2.1
    補上傳activity_login

--- app/src/main/java/com/example/tmdb_demo/LoginActivity.kt
+++ app/src/main/java/com/example/tmdb_demo/LoginActivity.kt
@@ -1,13 +1,19 @@
 package com.example.tmdb_demo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class LoginActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "Login"
     }
+
+    private var mGoogleSignInClient: GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
